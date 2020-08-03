82aa4b83250526409eb424447718e731ccadcafb
Przemysław Grondek <pgrondek@gmail.com>
2020-08-03 10:27:28 +0200

    Add config for Xephyr and Xnest

--- config.h
+++ config.h
@@ -36,6 +36,8 @@ static const Rule rules[] = {
     {"Virt-viewer", NULL, NULL, 1 << 8, 0, 0, 0, -1},
     {"Firefox", "Toolkit", "Picture-in-Picture", (1 << 9) - 1, 0, 0, 0, -1},
     {"st", NULL, NULL, 0, 0, 1, 1, -1},
+    {"Xephyr", NULL, NULL, 0, 1, 0, 0, -1},
+    {"Xnest", NULL, NULL, 0, 1, 0, 0, -1},
 };
 
 /* layout(s) */
