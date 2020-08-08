114e6ce3ce0f97a9444ca38c25dc855fbfe2a5ed
pgrondek <pgrondek@gmail.com>
2020-08-08 19:48:23 +0200

    Revert "Revert "Revert "Add columns layout"""
    This reverts commit 2ded22419e88c351345ce91d29509db8619e8ea1.

--- config.h
+++ config.h
@@ -46,8 +46,7 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
-    { "|||",      col },
-    { "><>",      NULL },    /* no layout function means floating behavior */
+	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 };
 
